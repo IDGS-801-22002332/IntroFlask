e39ef931f7c71ea6dfc49fd656d04a4c5657cb4a
IDGS-801-22002332 <jesusisaguirr@gmail.com>
2025-02-17 18:37:09 -0600

    17/02/2025 ZodiacoChinoFlask

--- main.py
+++ main.py
@@ -78,6 +78,34 @@ def calculadora():
 
     return render_template("OperasBas.html", resultado=resultado)
 
+@app.route("/cinepolis", methods=["GET", "POST"])
+def cinepolis():
+    total_pagar = None
+    nombre = ""
+    mensaje = ""
+    
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        cant_personas = int(request.form["cant_personas"])
+        cant_boletos = int(request.form["cant_boletos"])
+        cineco = request.form["cineco"]
+        precio_boleto = 12
+        
+        cant_maxima = cant_personas * 7
+        
+        if cant_boletos > cant_maxima:
+            mensaje = f"No puedes comprar más de {cant_maxima} boletos. Intenta de nuevo."
+        else:
+            total_pagar = cant_boletos * precio_boleto
+            
+            if cant_boletos > 5:
+                total_pagar *= 0.85  
+            
+            if cineco == "si":
+                total_pagar *= 0.90  
+                
+    return render_template("Cinepolis.html", total_pagar=total_pagar, nombre=nombre, mensaje=mensaje)
+
 def calcular_edad(anio):
     anio_actual = datetime.now().year
     return anio_actual - anio
